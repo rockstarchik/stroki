8c3e4474e241aece5bb85a26ef3bd2605193948a
rockstarchik <112154161+rockstarchik@users.noreply.github.com>
2022-11-21 15:05:13 +0300

    Add files via upload

---
+++ stroki.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef STROKIF_INCLUDED
+#define STROKIF_INCLUDED
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string itc_hello_str(string name);
+long long itc_len(string str);
+void itc_print_copy_str(string str, int number);
+
+#endif // STROKIF_INCLUDED
+
---
+++ strokif.cpp
@@ -0,0 +1,28 @@
+#include "stroki.h"
+using namespace std;
+
+string itc_hello_str(string name) {
+	return "Hello, " + name;
+}
+
+long long itc_len(string str) {
+	int kol, i;
+	kol = 0;
+	i = 0;
+	while (str[i] != '\0') {
+		kol++;
+		i++;
+	}
+	return kol;
+}
+
+void itc_print_copy_str(string str, int number) {
+	for(str = 1;str < number; str++)
+}
+
+void itc_first_end_three(string str) {
+	if (str[i] < 5)
+
+	
+
+}
